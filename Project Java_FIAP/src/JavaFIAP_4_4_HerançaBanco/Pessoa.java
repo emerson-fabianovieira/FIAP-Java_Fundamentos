06a1439179896bb0b145b414788b4aa3964385fb
emerson-fabianovieira <vieira.efv@gmail.com>
2022-05-16 11:52:31 -0300

    4.4 Herança Banco

---
+++ Project Java_FIAP/src/JavaFIAP_4_4_HerançaBanco/Pessoa.java
@@ -0,0 +1,21 @@
+package JavaFIAP_4_4_HerançaBanco;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome() {
+		this.nome = nome;
+	}
+
+}
