96d78671d2ff6d793f594eaa4a18e03bb7130535
emerson-fabianovieira <vieira.efv@gmail.com>
2022-05-19 15:22:24 -0300

    utilizado "while"

---
+++ Project Java_FIAP/src/JavaFIAP_5_1_Caixa/Caixa.java
@@ -0,0 +1,17 @@
+package JavaFIAP_5_1_Caixa;
+
+public class Caixa {
+
+	public static void main(String[] args) {
+		
+		int qtdProdutos = 5 ;
+		int registro = 0;
+		
+		while (registro < qtdProdutos) {
+			registro++;
+			System.out.println(" O produto número " +registro + " foi registrado");
+		}
+			
+	}
+
+}
