a1c3b2bebda63bb2f940439ee698b7d3215fcb41
emerson-fabianovieira <vieira.efv@gmail.com>
2022-05-19 15:32:23 -0300

    5.2 - Utilização " do while"

---
+++ Project Java_FIAP/src/JavaFIAP_5_2_Caixa/Caixa.java
@@ -0,0 +1,22 @@
+package JavaFIAP_5_2_Caixa;
+
+public class Caixa {
+
+	public static void main(String[] args) {
+		
+		int qtdProdutos = 5 ;
+		int registro = 0;
+		
+//		while (registro < qtdProdutos) {
+//			registro++;	
+//			System.out.println(" O produto número " +registro + " foi registrado");
+//		}
+		
+		do {
+			registro++;
+			System.out.println(" O caixa registrou o produto " + registro);
+		} while (registro < qtdProdutos);
+			
+	}
+
+}
