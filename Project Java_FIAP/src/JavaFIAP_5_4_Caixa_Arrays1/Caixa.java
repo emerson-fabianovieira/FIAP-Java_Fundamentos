9c64c0aa6eb6078f2555e2fe310b76bf7f9a746a
emerson-fabianovieira <vieira.efv@gmail.com>
2022-05-19 15:47:25 -0300

    JavaFIAP_5_4_Caixa_Arrays1

---
+++ Project Java_FIAP/src/JavaFIAP_5_4_Caixa_Arrays1/Caixa.java
@@ -0,0 +1,35 @@
+package JavaFIAP_5_4_Caixa_Arrays1;
+
+public class Caixa {
+
+	public static void main(String[] args) {
+		
+		int qtdProdutos = 5 ;
+		int registro = 0;
+		
+//		while (registro < qtdProdutos) {
+//			registro++;	
+//			System.out.println(" O produto número " +registro + " foi registrado");
+//		}
+		
+//		do {
+//			registro++;
+//			System.out.println(" O caixa registrou o produto " + registro);
+//		} while (registro < qtdProdutos);
+
+//		for (int i = 0 ; i < qtdProdutos; i++ ) {
+//			System.out.println(" O caixa registrou o produto " + i);
+//		}
+		
+		float[] valores = new float[5];
+		
+		valores[0] = 10;
+		valores[1] = 20;
+		valores[2] = 30;
+		valores[3] = 40;
+		valores[4] = 50;
+		
+		System.out.println(valores[1]);
+	}
+
+}
